5fab148532f404bd0e864cd5febdb8eb5c78a8a0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-05-12 09:36:58 +0200

    Write to output panel

--- src/Paket.VisualStudio/SolutionExplorer/PaketMenuCommandService.cs
+++ src/Paket.VisualStudio/SolutionExplorer/PaketMenuCommandService.cs
@@ -8,6 +8,7 @@ using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Paket.VisualStudio.Commands;
 using System.Threading.Tasks;
+using EnvDTE;
 
 namespace Paket.VisualStudio.SolutionExplorer
 {
@@ -16,6 +17,37 @@ namespace Paket.VisualStudio.SolutionExplorer
         private readonly IServiceProvider serviceProvider;
         private readonly OleMenuCommandService menuCommandService;
         private readonly ActiveGraphNodeTracker tracker;
+        private IVsOutputWindow _outputWindow;
+        private const string PaneName = "Paket";
+        private static readonly Guid PaneGuid = new Guid("A66E0A70-1A2A-4E3C-A806-1E537E608776");
+        private IVsOutputWindowPane _outputPane;
+
+        private IVsOutputWindowPane OutputPane
+        {
+            get
+            {
+                if (_outputPane == null)
+                {
+                    Guid generalPaneGuid = PaneGuid;
+                    IVsOutputWindowPane pane;
+
+                    OutputWindow.GetPane(ref generalPaneGuid, out pane);
+
+                    if (pane == null)
+                    {
+                        OutputWindow.CreatePane(ref generalPaneGuid, PaneName, 1, 1);
+                        OutputWindow.GetPane(ref generalPaneGuid, out pane);
+
+                        Paket.Logging.RegisterTraceFunction(text => { OutputPane.OutputStringThreadSafe(text + "\r\n"); });
+                    }
+
+                    _outputPane = pane;
+                }
+
+                return _outputPane;
+            }
+        }
+
 
         public PaketMenuCommandService(IServiceProvider serviceProvider, OleMenuCommandService menuCommandService, ActiveGraphNodeTracker tracker)
         {
@@ -24,9 +56,28 @@ namespace Paket.VisualStudio.SolutionExplorer
             this.tracker = tracker;
         }
 
+        private IVsOutputWindow OutputWindow
+        {
+            get
+            {
+                if (_outputWindow == null)
+                {
+                    DTE dte = (DTE)(this.serviceProvider.GetService(typeof(DTE)));
+                    IServiceProvider serviceProvider = new ServiceProvider(dte as Microsoft.VisualStudio.OLE.Interop.IServiceProvider);
+                    _outputWindow = serviceProvider.GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+                }
+
+                return _outputWindow;
+            }
+        }
+
         private void UpdatePackageAsync(string dependenciesFileName, string packageName)
-        {            
-            System.Threading.Tasks.Task.Run(() => {
+        {
+            OutputPane.Activate();
+
+            System.Threading.Tasks.Task.Run(() =>
+            {                
+                OutputPane.OutputStringThreadSafe(String.Format("Updating {0}\r\n", packageName));
                 Paket.Dependencies.Locate(dependenciesFileName)
                     .UpdatePackage(packageName, Microsoft.FSharp.Core.FSharpOption<string>.None, false, false);
             });
