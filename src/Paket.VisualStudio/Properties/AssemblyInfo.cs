2dea2475462823a2b6cc5fe3ed98bcc8d954fdda
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-11-26 13:37:08 +0100

    Bump version to 0.8.2

--- src/Paket.VisualStudio/Properties/AssemblyInfo.cs
+++ src/Paket.VisualStudio/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Paket.VisualStudio")]
 [assembly: AssemblyProductAttribute("Paket.VisualStudio")]
 [assembly: AssemblyDescriptionAttribute("Manage your Paket dependencies from Visual Studio!")]
-[assembly: AssemblyVersionAttribute("0.8.0")]
-[assembly: AssemblyFileVersionAttribute("0.8.0")]
+[assembly: AssemblyVersionAttribute("0.8.2")]
+[assembly: AssemblyFileVersionAttribute("0.8.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.8.0";
+        internal const string Version = "0.8.2";
     }
 }
