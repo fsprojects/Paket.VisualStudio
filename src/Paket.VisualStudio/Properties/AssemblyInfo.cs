23f8bba180048ab514f7d9033e3eb60347149e09
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-01-16 11:40:15 +0100

    Bump version to 0.13.0

--- src/Paket.VisualStudio/Properties/AssemblyInfo.cs
+++ src/Paket.VisualStudio/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Paket.VisualStudio")]
 [assembly: AssemblyProductAttribute("Paket.VisualStudio")]
 [assembly: AssemblyDescriptionAttribute("Manage your Paket dependencies from Visual Studio!")]
-[assembly: AssemblyVersionAttribute("0.12.1")]
-[assembly: AssemblyFileVersionAttribute("0.12.1")]
+[assembly: AssemblyVersionAttribute("0.13.0")]
+[assembly: AssemblyFileVersionAttribute("0.13.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.12.1";
+        internal const string Version = "0.13.0";
     }
 }
