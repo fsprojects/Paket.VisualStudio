0d92eb6e37eea7ddc7d6b87aed63b3266255b6f3
Ruh Ullah Shah <ruhullahshah88@gmail.com>
2017-10-12 13:08:56 +0200

    Verify if .paket\paket.exe exists if not, try downloading using .paket\paket.bootstrapper.exe else fail

--- src/Paket.VisualStudio/Utils/PaketLauncher.cs
+++ src/Paket.VisualStudio/Utils/PaketLauncher.cs
@@ -1,25 +1,57 @@
 ﻿using System.Diagnostics;
+using System.IO;
 
 namespace Paket.VisualStudio.Utils
 {
     public static class PaketLauncher
     {
+        const string PAKET_EXE = ".paket\\paket.exe";
+        const string PAKET_BOOTSTRAPPER_EXE = ".paket\\paket.bootstrapper.exe";
+        const string PAKET_RELEASE_URL = "https://github.com/fsprojects/Paket/releases";
+        const string PAKET_GETTING_STARTED_URL = "https://fsprojects.github.io/Paket/getting-started.html";
+
+        private static int LaunchProcess(string SolutionDirectory, string FileName, string PaketSubCommand, DataReceivedEventHandler PaketDataReceivedHandler)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = SolutionDirectory + FileName;
+            process.StartInfo.Arguments = PaketSubCommand;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WorkingDirectory = SolutionDirectory;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += PaketDataReceivedHandler;
+            process.ErrorDataReceived += PaketDataReceivedHandler;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+
         public static void LaunchPaket(string SolutionDirectory, string PaketSubCommand, DataReceivedEventHandler PaketDataReceivedHandler)
         {
-                Process process = new Process();
-                process.StartInfo.FileName = SolutionDirectory + ".paket\\paket.exe";
-                process.StartInfo.Arguments = PaketSubCommand;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.WorkingDirectory = SolutionDirectory;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.OutputDataReceived += PaketDataReceivedHandler;
-                process.ErrorDataReceived += PaketDataReceivedHandler;
-                process.Start();
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-                process.WaitForExit();
+            if (!File.Exists(SolutionDirectory + PAKET_EXE))
+            {
+                //If .paket\paket.exe is not found under the solution dir, try launching paket.bootstrapper.exe
+                if (File.Exists(SolutionDirectory + PAKET_BOOTSTRAPPER_EXE))
+                {
+                    int ExitCode = LaunchProcess(SolutionDirectory, PAKET_BOOTSTRAPPER_EXE, "", PaketDataReceivedHandler);
+                    if (ExitCode != 0)
+                        /* If something went wrong with paket.bootstrapper.exe e.g. couldn't download paket.exe due to proxy auth error
+                         * then we should not execute the command that was originally issued for paket.exe.
+                         */
+                        throw new System.Exception("paket.bootstrapper.exe terminated abnormally.");
+                }
+                else
+                    throw new FileNotFoundException(
+                        @"Could not locate .paket\paket.exe and .paket\paket.bootstrapper.exe under the solution " + SolutionDirectory
+                        + "\nTo download the binaries, visit " + PAKET_RELEASE_URL
+                        + "\nTo know more about Paket, visit " + PAKET_GETTING_STARTED_URL + "\n");
+            }
+            /* At this point, all is well .paket\paket.exe exists or .paket\paket.bootstrapper.exe downloaded it successfully.
+             * Now issue the original command to .paket\paket.exe
+             */
+            LaunchProcess(SolutionDirectory, PAKET_EXE, PaketSubCommand, PaketDataReceivedHandler);
         }
     }
-    
 }
