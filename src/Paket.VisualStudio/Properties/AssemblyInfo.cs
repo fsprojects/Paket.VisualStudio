43b79a93a1d8803e3c11d43aa9fbfb1fa84cb3bb
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-12 16:35:03 +0200

    Bump version to 0.34.0

--- src/Paket.VisualStudio/Properties/AssemblyInfo.cs
+++ src/Paket.VisualStudio/Properties/AssemblyInfo.cs
@@ -6,15 +6,15 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Paket.VisualStudio")]
 [assembly: AssemblyProductAttribute("Paket.VisualStudio")]
 [assembly: AssemblyDescriptionAttribute("Manage your Paket dependencies from Visual Studio!")]
-[assembly: AssemblyVersionAttribute("0.33.3")]
-[assembly: AssemblyFileVersionAttribute("0.33.3")]
+[assembly: AssemblyVersionAttribute("0.34.0")]
+[assembly: AssemblyFileVersionAttribute("0.34.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String InternalsVisibleTo = "Paket.VisualStudio.Tests";
         internal const System.String AssemblyTitle = "Paket.VisualStudio";
         internal const System.String AssemblyProduct = "Paket.VisualStudio";
         internal const System.String AssemblyDescription = "Manage your Paket dependencies from Visual Studio!";
-        internal const System.String AssemblyVersion = "0.33.3";
-        internal const System.String AssemblyFileVersion = "0.33.3";
+        internal const System.String AssemblyVersion = "0.34.0";
+        internal const System.String AssemblyFileVersion = "0.34.0";
     }
 }
